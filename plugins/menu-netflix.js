675aa2de317f00a726c7eaf553653b0db68e417e
King123XZ <yersonorosco2002@gmail.com>
2025-06-26 15:49:05 -0500

    Create menu-netflix.js

---
+++ plugins/menu-netflix.js
@@ -0,0 +1,46 @@
+const handler = async (m, { conn, usedPrefix, command }) => {
+  const texto = `
+🌐 *NETFLIX* 🚀
+
+
+─
+📌 Usa *.menu* para ver todos los comandos.
+`.trim() + "\n\n🔹 Selecciona una opción:";
+
+  // Botones personalizados
+  const buttons = [
+    {
+      buttonId: `${usedPrefix}COMPRAR`,
+      buttonText: { displayText: "C " },
+      type: 1,
+    },
+    {
+      buttonId: `${usedPrefix}LISTADECUENTASENVENTAS`,
+      buttonText: { displayText: "ATRAS " },
+      type: 1,
+    },
+  ];
+
+  await conn.sendMessage(m.chat, {
+    text: texto,
+    footer: "NagiBotV3",
+    buttons: buttons,
+    headerType: 1,
+    contextInfo: {
+      externalAdReply: {
+        title: "NagiBotV2 🌌",
+        body: "Únete a nuestros grupos oficiales",
+        thumbnailUrl: 'https://qu.ax/hcjnp.jpg',
+        sourceUrl: "https://github.com/El-brayan502/NagiBotV2",
+        mediaType: 1,
+        renderLargerThumbnail: true
+      }
+    }
+  }, { quoted: m })
+}
+
+handler.help = ['NETFLIX']
+handler.tags = ['CUENTAS']
+handler.command = /^NETFLIX$/i
+
+export default handler
