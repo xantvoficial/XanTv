52d128f9b2ef73a82e09ac1deccc54799a7a9e4a
King123XZ <yersonorosco2002@gmail.com>
2025-06-27 22:40:48 -0500

    Update menu-principal.js

--- plugins/menu-principal.js
+++ plugins/menu-principal.js
@@ -22,7 +22,7 @@
       type: 1,
     },
     {
-      buttonId: `${usedPrefix}code`,
+      buttonId: `${usedPrefix}.grupooficial`,
       buttonText: { displayText: "GRUPO OFICAL" },
       type: 1,
     },
