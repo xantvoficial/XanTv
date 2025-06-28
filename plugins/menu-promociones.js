97ab4328cb0b52b04c85293217f5c865d9389c34
King123XZ <yersonorosco2002@gmail.com>
2025-06-27 23:32:43 -0500

    Update menu-promociones.js

--- plugins/menu-promociones.js
+++ plugins/menu-promociones.js
@@ -1,22 +1,39 @@
 const handler = async (m, { conn, usedPrefix, command }) => {
-  const buttons = [
-    {
-      buttonId: `${usedPrefix}menu`,
-      buttonText: { displayText: "🔙 Volver" },
-      type: 1,
-    },
-  ];
+const texto = `
+hola
 
-  await conn.sendMessage(m.chat, {
-    image: { url: 'https://qu.ax/WSBNA.jpg' }, // Ensure this URL is valid
-    footer: "🔐 XanTV - dv yer",
-    buttons: buttons,
-    headerType: 4,
-  }, { quoted: m });
+🔹 Selecciona una opción:
+`.trim();
+
+const buttons = [
+
+{
+buttonId: ${usedPrefix}LISTADECUENTASENVENTAS,
+buttonText: { displayText: "🔙 Volver" },
+type: 1,
+},
+];
+
+await conn.sendMessage(m.chat, {
+image: { url: 'https://qu.ax/WSBNA.jpg' },
+caption: texto,
+footer: "🔐 XanTV - dv yer",
+buttons: buttons,
+headerType: 4,
+contextInfo: {
+externalAdReply: {
+title: "🎥 XanTV - dv yer 🌌",
+body: "hola",
+sourceUrl: "https://github.com/king123XZ/XanTv", // Puedes cambiar este enlace por tu repo si tienes uno
+mediaType: 1,
+renderLargerThumbnail: true
+}
+}
+}, { quoted: m })
 }
 
 //handler.help = ['PROMOCIONES']
 //handler.tags = ['CUENTAS']
-handler.command = /^PROMOCIONES$/i
+handler.command = /^PROMOCIONES\$/i
 
-export default handler;
+export default handler que solo envie la imagen y solo deja el boton de volver
