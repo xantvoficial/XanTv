39eb750d71bcd3d6e1df4f2d8057e601743f5885
King123XZ <yersonorosco2002@gmail.com>
2025-06-27 23:12:39 -0500

    Update menu-grupo.js

--- plugins/menu-grupo.js
+++ plugins/menu-grupo.js
@@ -9,15 +9,21 @@ const handler = async (m, { conn }) => {
 📺 *LIVE* en TikTok 
 
 💎 *¡Únete para ser Cliente VIP!* 💎
-
-🔗 *Enlace del grupo:*
-https://chat.whatsapp.com/Fu828nubauZ4Kk4fE1IC1d
 `.trim();
 
   await conn.sendMessage(m.chat, {
     image: { url: 'https://qu.ax/GKVqa.jpg' },
     caption: texto,
-    footer: "🔐 XanTv - creador dv yer"
+    footer: "🔐 XanTv - creador dv yer",
+    buttons: [
+      {
+        buttonId: 'enlace_grupo',
+        buttonText: { displayText: '🔗 Unirme al grupo VIP' },
+        type: 1,
+        url: 'https://chat.whatsapp.com/Fu828nubauZ4Kk4fE1IC1d' // Esto NO funciona en todos los frameworks, ver nota abajo
+      }
+    ],
+    headerType: 4
   }, { quoted: m });
 };
 
