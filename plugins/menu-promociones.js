d14d2ca3df414d44b5b3901b8ba2501748df7a0d
King123XZ <yersonorosco2002@gmail.com>
2025-06-27 23:35:55 -0500

    Update menu-promociones.js

--- plugins/menu-promociones.js
+++ plugins/menu-promociones.js
@@ -1,39 +1,29 @@
-const handler = async (m, { conn, usedPrefix, command }) => {
-const texto = `
-hola
+const handler = async (m, { conn, usedPrefix }) => {
+  const texto = `
+📢 *Promoción activa*
 
-🔹 Selecciona una opción:
+Revisa nuestras cuentas disponibles 👇
 `.trim();
 
-const buttons = [
+  const buttons = [
+    {
+      buttonId: `${usedPrefix}LISTADECUENTASENVENTAS`,
+      buttonText: { displayText: "🔙 Volver" },
+      type: 1,
+    },
+  ];
 
-{
-buttonId: ${usedPrefix}LISTADECUENTASENVENTAS,
-buttonText: { displayText: "🔙 Volver" },
-type: 1,
-},
-];
-
-await conn.sendMessage(m.chat, {
-image: { url: 'https://qu.ax/WSBNA.jpg' },
-caption: texto,
-footer: "🔐 XanTV - dv yer",
-buttons: buttons,
-headerType: 4,
-contextInfo: {
-externalAdReply: {
-title: "🎥 XanTV - dv yer 🌌",
-body: "hola",
-sourceUrl: "https://github.com/king123XZ/XanTv", // Puedes cambiar este enlace por tu repo si tienes uno
-mediaType: 1,
-renderLargerThumbnail: true
-}
-}
-}, { quoted: m })
+  await conn.sendMessage(m.chat, {
+    image: { url: 'https://qu.ax/WSBNA.jpg' },
+    caption: texto,
+    footer: "🔐 XanTV - dv yer",
+    buttons: buttons,
+    headerType: 4
+  }, { quoted: m });
 }
 
-//handler.help = ['PROMOCIONES']
-//handler.tags = ['CUENTAS']
-handler.command = /^PROMOCIONES\$/i
+handler.help = ['PROMOCIONES'];
+handler.tags = ['info'];
+handler.command = /^PROMOCIONES$/i;
 
-export default handler que solo envie la imagen y solo deja el boton de volver
+export default handler;
