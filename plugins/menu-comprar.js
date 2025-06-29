1369cab30e0aabd27ee811a6e4a929eb6b4ec759
King123XZ <yersonorosco2002@gmail.com>
2025-06-28 22:05:26 -0500

    Update menu-comprar.js

--- plugins/menu-comprar.js
+++ plugins/menu-comprar.js
@@ -1,16 +1,8 @@
 let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, {
-    image: { url: 'https://tinyurl.com/26hgmdfm' },
-    caption: `🎬 *VENTA DE CUENTAS* 🔥
-
-🔥 Netflix, Disney+, Spotify y más.
-💰 Precios accesibles y servicio garantizado.
-📆 Duración mínima de 30 días.
-
-📞 Contáctanos para más detalles.`,
+    image: { url: 'https://tinyurl.com/26hgmdfm' }
   }, { quoted: m })
 }
 
 handler.command = ['owner', 'creador', 'dueño', 'comprarcuenta']
 export default handler
-
