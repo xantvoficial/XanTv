812d7acbe0fabc85199486330fcb61048288483a
King123XZ <yersonorosco2002@gmail.com>
2025-08-01 20:42:00 -0500

    Actualizar menu-promociones.js

--- plugins/menu-promociones.js
+++ plugins/menu-promociones.js
@@ -19,7 +19,7 @@ Revisa nuestras cuentas disponibles 👇
   ];
 
   await conn.sendMessage(m.chat, {
-    image: { url: 'https://qu.ax/KNlQv.jpg' },
+    image: { url: 'https://i.ibb.co/cc71nFpB/IMG-20250801-WA0064.jpg' },
     caption: texto,
     footer: "🔐 XanTV - dv yer",
     buttons: buttons,
