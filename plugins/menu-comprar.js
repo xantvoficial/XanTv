69c7a3aaf59968c806f3b1e7cfca2428a5d23a08
xantvoficial <xantvoficial.12@gmail.com>
2025-08-07 01:50:09 -0500

    Update menu-comprar.js

--- plugins/menu-comprar.js
+++ plugins/menu-comprar.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, {
-    image: { url: '(https://i.postimg.cc/hPQhFmNc/2.png)' }
+    image: {url: 'https://i.postimg.cc/hPQhFmNc/2.png'}
   }, { quoted: m })
 }
 
