c34c4a8d803899665ee4af7a8a4e4545cf037972
King123XZ <yersonorosco2002@gmail.com>
2025-08-07 17:18:49 -0500

    Update menu-referencias.js

--- plugins/menu-referencias.js
+++ plugins/menu-referencias.js
@@ -1,18 +1,26 @@
 const handler = async (m, { conn, usedPrefix, command }) => {
   // Definir el texto que se enviará
   const texto = `
-
 El buen servicio que brindo a todos los clientes que adquieren mis plataformas se demuestra en mi página! 🔍
 
-📲 https://www.instagram.com/xann.tv 
+📲 https://www.instagram.com/xann.tv
 
-¡Verifica por ti mismo nuestra trayectoria!
+¡Verifica por ti mismo nuestra trayectoria!
+  `.trim();
 
-  // Enviar solo el texto
+  // URL de la imagen que se enviará
+  const imageUrl = 'https://i.postimg.cc/nzhZZscH/VENTAS.png'; // Cambia esta URL por la de la imagen que deseas enviar
+
+  // Enviar el texto
   await conn.sendMessage(m.chat, { text: texto });
+
+  // Enviar la imagen
+  await conn.sendMessage(m.chat, {
+    image: { url: imageUrl }, // URL de la imagen
+    caption: '¡Verifica nuestros servicios en Instagram!' // Opcional, texto debajo de la imagen
+  });
 }
 
-handler.command = /^referencias$/i  // El comando que activará el envío de texto
+handler.command = /^referencias$/i  // El comando que activará el envío de texto y la imagen
 
 export default handler;
-
