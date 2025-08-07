6ddd47759aed4ba128ee2851dc5c9e6e8ca3923b
King123XZ <yersonorosco2002@gmail.com>
2025-08-07 16:47:55 -0500

    Update menu-otrascuentas.js

--- plugins/menu-otrascuentas.js
+++ plugins/menu-otrascuentas.js
@@ -2,7 +2,7 @@ const handler = async (m, { conn, usedPrefix, command }) => {
   // Enviar solo la imagen
   await conn.sendMessage(m.chat, {
     image: { url: 'https://i.postimg.cc/hPQhFmNc/2.png' }, 
-    caption: '🎥 *Imagen solicitada* 🎥', 
+    caption: 'Gracias', 
     footer: "🔐 XanTV - dv yer", 
   }, { quoted: m })
 }
