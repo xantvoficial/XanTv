9cb673e8bfe1b5337565a5ae30fcf5536c2b6413
xantvoficial <xantvoficial.12@gmail.com>
2025-08-07 02:35:17 -0500

    Update menu-otrascuentas.js

--- plugins/menu-otrascuentas.js
+++ plugins/menu-otrascuentas.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, {
     image: {url: 'https://i.postimg.cc/hPQhFmNc/2.png'}
   }, { quoted: m })
-  
+  }
 
 handler.command = ['owner', 'creador', 'dueño', 'comprarcuenta']
 export default handler
