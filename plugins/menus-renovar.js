d78314af423f8ae782cc32c5fee30264b20d5b4a
xantvoficial <xantvoficial.12@gmail.com>
2025-08-07 18:30:23 -0500

    Update menus-renovar.js

--- plugins/menus-renovar.js
+++ plugins/menus-renovar.js
@@ -2,7 +2,7 @@ const handler = async (m, { conn, usedPrefix, command }) => {
   const texto = `
 Recuerda enviarnos captura! 👩🏻‍💻
 `.trim();
-  
+},
 
   await conn.sendMessage(m.chat, {
     image: {url: 'https://i.postimg.cc/wxFHWQ27/RENOVACIONES.png'},
