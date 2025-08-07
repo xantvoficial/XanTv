358afb8d63723f65f2fa524dfb4457621ac52849
King123XZ <yersonorosco2002@gmail.com>
2025-08-07 16:51:06 -0500

    Update menu-comprar.js

--- plugins/menu-comprar.js
+++ plugins/menu-comprar.js
@@ -17,7 +17,7 @@ Revisa nuestras cuentas disponibles 👇
       type: 1,
     },
     {
-      buttonId: `${usedPrefix}owner`,
+      buttonId: `${usedPrefix}metododepago`,
       buttonText: { displayText: "💰 Método de Pago" },
       type: 1,
     },
