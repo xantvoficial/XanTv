e200a8f58606ae59a9841530c73c0abc94737ed5
King123XZ <yersonorosco2002@gmail.com>
2025-08-07 16:26:49 -0500

    Update menu-otrascuentas.js

--- plugins/menu-otrascuentas.js
+++ plugins/menu-otrascuentas.js
@@ -1,8 +1,1 @@
-let handler = async (m, { conn }) => {
-  await conn.sendMessage(m.chat, {
-    image: {url: 'https://i.postimg.cc/hPQhFmNc/2.png'}
-  }, { quoted: m })
-  }
-
-handler.command = ['owner', 'creador', 'dueño', 'comprarcuenta']
-export default handler
+//
