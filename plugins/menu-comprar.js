5d70a877fdacc3bb9d4798b2c876d92ba191d51b
xantvoficial <xantvoficial.12@gmail.com>
2025-08-07 02:17:02 -0500

    Update menu-comprar.js

--- plugins/menu-comprar.js
+++ plugins/menu-comprar.js
@@ -3,15 +3,7 @@ let handler = async (m, { conn }) => {
     image: {url: 'https://i.postimg.cc/hPQhFmNc/2.png'}
   }, { quoted: m })
   
-🔹 Selecciona una opción:
-`.trim();
-
-  const buttons = [
-    {
-      buttonId: `${usedPrefix}📌OtrasCuentas`,
-      buttonText: { displayText: "🛍 Métodos de Pago" },
-      type: 1,
-    },
+}
 
 handler.command = ['owner', 'creador', 'dueño', 'comprarcuenta']
 export default handler
