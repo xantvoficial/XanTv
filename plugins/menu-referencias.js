1ad895f4854eefccc704037947df2d1755f35cc1
King123XZ <yersonorosco2002@gmail.com>
2025-08-07 17:11:46 -0500

    Update menu-referencias.js

--- plugins/menu-referencias.js
+++ plugins/menu-referencias.js
@@ -1,4 +1,4 @@
-const { WAConnection, MessageType } = require('@whiskeysockets/baileys');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
 const qrcode = require('qrcode-terminal');
 
 // Crear una nueva conexión de WhatsApp
@@ -16,47 +16,30 @@ conn.on('ready', () => {
 
 // Handler para el comando de carrusel de imágenes
 conn.on('message-new', async (m) => {
-    if (m.message && m.message.conversation === '/referencias') {
+    if (m.message && m.message.conversation === '/ellanoteama') {
         // Enlaces de las 7 imágenes para el carrusel
         const imageUrls = [
-            'https://i.postimg.cc/nzhZZscH/VENTAS.png',
-            'https://i.postimg.cc/4ydfrbdw/VENTAS2.png',
-            'https://i.postimg.cc/rygd8r76/VENTAS3.png',
-            'https://i.postimg.cc/rygd8r76/VENTAS4.png',
-            'https://i.postimg.cc/rygd8r76/VENTAS5.png',
-            'https://i.postimg.cc/rygd8r76/VENTAS6.png',
-            'https://i.postimg.cc/rygd8r76/VENTAS7.png'
+            'https://i.postimg.cc/nzhZZscH/VENTAS.png', // Imagen 1
+            'https://i.postimg.cc/4ydfrbdw/VENTAS2.png', // Imagen 2
+            'https://i.postimg.cc/rygd8r76/VENTAS3.png', // Imagen 3
+            'https://i.postimg.cc/rygd8r76/VENTAS4.png', // Imagen 4
+            'https://i.postimg.cc/rygd8r76/VENTAS5.png', // Imagen 5
+            'https://i.postimg.cc/rygd8r76/VENTAS6.png', // Imagen 6
+            'https://i.postimg.cc/rygd8r76/VENTAS7.png'  // Imagen 7
         ];
 
-        // Enviar las imágenes secuencialmente
+        // Enviar las imágenes secuenciales
         for (let i = 0; i < imageUrls.length; i++) {
             await conn.sendMessage(m.key.remoteJid, {
-                image: { url: imageUrls[i] }, // URL de la imagen
+                image: { url: imageUrls[i] }, // Enviar la imagen desde el URL
                 caption: `Imagen ${i + 1}`, // Título de la imagen
                 footer: '🔐 XanTV - dv yer', // Pie de página
             }, MessageType.text);
         }
-
-        // Después de enviar las imágenes, ofrecer la opción de volver al menú o ver más imágenes
-        await conn.sendMessage(m.key.remoteJid, {
-            text: 'Selecciona una opción:',
-            footer: '🔐 XanTV - dv yer',
-            buttons: [
-                {
-                    buttonId: '/metododepago',
-                    buttonText: { displayText: '🛍️ Ver más imágenes' },
-                    type: 1
-                },
-                {
-                    buttonId: '/menu',
-                    buttonText: { displayText: '🔙 Volver al menú' },
-                    type: 1
-                }
-            ]
-        });
     }
 });
 
 // Iniciar la conexión
 conn.connect();
+
 
