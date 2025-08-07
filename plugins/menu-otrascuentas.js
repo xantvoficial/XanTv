3a32e98ee831acec5aadd0eb55cb81351d942cff
xantvoficial <xantvoficial.12@gmail.com>
2025-08-07 18:10:15 -0500

    Update menu-otrascuentas.js

--- plugins/menu-otrascuentas.js
+++ plugins/menu-otrascuentas.js
@@ -2,7 +2,7 @@ const handler = async (m, { conn, usedPrefix, command }) => {
   // Enviar solo la imagen
   await conn.sendMessage(m.chat, {
     image: { url: 'https://i.postimg.cc/hPQhFmNc/2.png' }, 
-    caption: 'Gracias', 
+    caption: '', 
     footer: "🔐 XanTV - dv yer", 
   }, { quoted: m })
 }
