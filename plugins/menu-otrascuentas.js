5683bf1a5ad5669c8faf23ff74b93d4c42f64057
King123XZ <yersonorosco2002@gmail.com>
2025-08-12 21:09:01 -0500

    Actualizar menu-otrascuentas.js

--- plugins/menu-otrascuentas.js
+++ plugins/menu-otrascuentas.js
@@ -1,9 +1,11 @@
+import fs from 'fs'
+
 const handler = async (m, { conn, usedPrefix, command }) => {
   // Enviar solo la imagen
   await conn.sendMessage(m.chat, {
-    image: { url: 'https://i.postimg.cc/hPQhFmNc/2.png' }, 
-    caption: '', 
-    footer: "🔐 XanTV - dv yer", 
+    image: fs.readFileSync('./imagenes/otrascuentas.jpg'),
+    caption: '',
+    footer: "🔐 XanTV - dv yer",
   }, { quoted: m })
 }
 
