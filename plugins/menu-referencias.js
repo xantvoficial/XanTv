a210a6dad78d3b18e70a48ce1390c47bbe4a53b5
King123XZ <yersonorosco2002@gmail.com>
2025-08-12 21:04:27 -0500

    Actualizar menu-referencias.js

--- plugins/menu-referencias.js
+++ plugins/menu-referencias.js
@@ -1,8 +1,6 @@
-const handler = async (m, { conn, usedPrefix, command }) => {
-  // URL de la imagen que se enviará
-  const imageUrl = 'https://i.postimg.cc/RZ4mh03v/Imagen-de-Whats-App-2025-08-07-a-las-17-21-18-1ec7b9ae.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
+import fs from 'fs'
 
-  // Definir el texto que se enviará junto con la imagen
+const handler = async (m, { conn, usedPrefix, command }) => {
   const texto = `
 El buen servicio que brindo a todos los clientes que adquieren mis plataformas se demuestra en mi página! 🔍
 
@@ -11,13 +9,12 @@ El buen servicio que brindo a todos los clientes que adquieren mis plataformas s
 ¡Verifica por ti mismo nuestra trayectoria!
   `.trim();
 
-  // Enviar la imagen con el texto en el caption
   await conn.sendMessage(m.chat, {
-    image: { url: imageUrl }, // URL de la imagen
-    caption: texto // El texto va junto con la imagen
+    image: fs.readFileSync('./imagenes/referencia.jpg'),
+    caption: texto
   });
 }
 
-handler.command = /^referencias$/i  // El comando que activará el envío de texto y la imagen
+handler.command = /^referencias$/i
 
-export default handler;
+export default handler
