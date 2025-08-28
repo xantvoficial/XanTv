02795268f37c77384aa5839a2ec5e2d560b113eb
King123XZ <yersonorosco2002@gmail.com>
2025-08-28 08:24:07 -0500

    Update menu-principal.js

--- plugins/menu-principal.js
+++ plugins/menu-principal.js
@@ -1,13 +1,14 @@
+// plugins/menu.js
 import fs from 'fs'
 
-const handler = async (m, { conn, usedPrefix, command }) => {
+const handler = async (m, { conn, usedPrefix }) => {
   const texto = `
-*📽️CUENTAS STREAMING📽️*
+*📽️ CUENTAS STREAMING 📽️*
 
 🎬 *Servicios disponibles:*
    ┗ 📺 Netflix desde S/ 15.00
    ┗ 🤖 Disney desde S/ 7.00
-   ┗ 🎞️ HBO, Movistar, DirecTv, vix, Crunchyroll y más…
+   ┗ 🎞️ HBO, Movistar, DirecTv, Vix, Crunchyroll y más…
 
 ⏳ *Duración del servicio:* 
    ┗ 📆 Garantía de *30 días*
@@ -19,41 +20,22 @@ const handler = async (m, { conn, usedPrefix, command }) => {
    ┗ 🏦 Transferencias
 
 🔹 Selecciona una opción:
-`.trim();
-
-  const buttons = [
-    {
-      buttonId: `${usedPrefix}PROMOCIONES`,
-      buttonText: { displayText: "🛍 Comprar" },
-      type: 1,
-    },
-    {
-      buttonId: `${usedPrefix}grupooficial`,
-      buttonText: { displayText: "💎 Grupo Vip" },
-      type: 1,
-    },
-    {
-      buttonId: `${usedPrefix}renovar`,
-      buttonText: { displayText: "🛒 Renovar" },
-      type: 1,
-    },
-    {
-      buttonId: `${usedPrefix}referencias`,
-      buttonText: { displayText: "🔍 Referencias" },
-      type: 1,
-    },
-  ];
+`.trim()
 
   await conn.sendMessage(m.chat, {
     image: fs.readFileSync('./imagenes/imagen-menu.jpg'),
     caption: texto,
-    footer: "🔐 XanTv - creador dv yer",
-    buttons: buttons,
-    headerType: 4,
+    footer: '🔐 XanTv - creador dv yer',
+    templateButtons: [
+      { index: 1, quickReplyButton: { displayText: '🛍 Comprar',      id: `${usedPrefix}promociones` } },
+      { index: 2, quickReplyButton: { displayText: '💎 Grupo Vip',     id: `${usedPrefix}grupooficial` } },
+      { index: 3, quickReplyButton: { displayText: '🛒 Renovar',       id: `${usedPrefix}renovar` } },
+      { index: 4, quickReplyButton: { displayText: '🔍 Referencias',   id: `${usedPrefix}referencias` } },
+    ],
     contextInfo: {
       externalAdReply: {
-        title: "XanTv 🌌",
-        body: "Calidad, confianza y buen precio. Creador: dv yer",
+        title: 'XanTv 🌌',
+        body: 'Calidad, confianza y buen precio. Creador: dv yer',
         thumbnailUrl: 'https://qu.ax/GKVqa.jpg',
         mediaType: 1,
         renderLargerThumbnail: true
